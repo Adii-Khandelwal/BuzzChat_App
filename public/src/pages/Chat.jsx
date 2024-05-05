7bc03bedd8f4a49e07d5943f13cc155529aab9d1
aditya01khandelwal <aditya.khandelwal@getvisitapp.com>
2024-05-05 23:38:49 +0530

    added welcome page

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import {allUsersRoute} from "../utils/APIRoutes";
 import Contacts from "../components/Contacts"
+import Welcome from '../components/Welcome';
 
 function Chat() {
   const navigate = useNavigate();
@@ -53,6 +54,7 @@ async function settingContacts(){
    <Container>
     <div className="container">
     <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+    <Welcome currentUser = {currentUser}/>
     </div>
    </Container>
   )
