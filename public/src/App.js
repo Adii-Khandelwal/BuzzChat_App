f121ea6328c8d14fb0b55e219f9a5b2127a6cdbc
aditya01khandelwal <aditya.khandelwal@getvisitapp.com>
2024-05-07 11:45:40 +0530

    added lazy suspense

--- public/src/App.js
+++ public/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React,{Suspense, lazy} from 'react';
 import {BrowserRouter,Route,Routes } from 'react-router-dom';
-import Register from "./pages/Register";
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import SetAvatar from './pages/setAvatar';
+// import Register from "./pages/Register";
+// import Login from './pages/Login';
+// import Chat from './pages/Chat';
+// import SetAvatar from './pages/setAvatar';
 
+const Chat = lazy(()=> import("./pages/Chat"));
+const Login = lazy(()=> import("./pages/Login"));
+const Register = lazy(()=> import("./pages/Register"));
+const SetAvatar = lazy(()=>import("./pages/setAvatar"));
 export default function App() {
   return <BrowserRouter>
+  <Suspense fallback={<></>}>
   <Routes>
     <Route path="/register" element={<Register />}/>
     <Route path="/login" element={<Login />}/>
     <Route path="/" element={<Chat/>}/>
     <Route path="/setAvatar" element={<SetAvatar/>}/>
-
   </Routes>
+  </Suspense>
   </BrowserRouter>
 }
